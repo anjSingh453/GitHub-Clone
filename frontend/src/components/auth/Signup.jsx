50a8f9614b748e6d6807ff38fb1d61929d47c9c2
anjSingh453 <166209384+anjSingh453@users.noreply.github.com>
2025-05-13 22:45:07 +0530

    Update Signup.jsx

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -22,7 +22,7 @@ const Signup = () => {
 
     try {
       setLoading(true);
-      const res = await axios.post("http://localhost:3000/signup", {
+      const res = await axios.post("https://github-clone-xa4z.onrender.com/signup", {
         email: email,
         password: password,
         username: username,
