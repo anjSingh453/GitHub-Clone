6a0ce5173394d5e128b2504722de67070fa90c7e
anjSingh453 <166209384+anjSingh453@users.noreply.github.com>
2025-05-13 22:45:47 +0530

    Update Login.jsx

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
 
     try {
       setLoading(true);
-      const res = await axios.post("http://localhost:3000/login", {
+      const res = await axios.post("https://github-clone-xa4z.onrender.com/login", {
         email: email,
         password: password,
       });
